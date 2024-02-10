c4e12e472c9178a4d17b527aa1105715d48b004e
Shobhnik13 <shobhnikw@gmail.com>
2024-02-10 12:17:28 +0530

    initial

---
+++ index.ts
@@ -0,0 +1,46 @@
+declare var require:any
+const express=require('express')
+const app=express()
+const http=require('http')
+const server=http.createServer(app)
+import { Server } from "socket.io";
+
+const io = new Server(server,{ 
+    cors:{
+        //allowingf anyone on the server we created as io 
+        origin:"*",
+    }
+});
+
+type Point={
+    x:number,
+    y:number;
+}
+
+type Drawline={
+    prevPoint:null | Point,
+    currentPoint: Point,
+    color:string,
+}
+
+io.on("connection", (socket) => {
+    socket.on('client-ready',()=>{
+        socket.broadcast.emit('get-canvas-state')
+      })
+  
+      socket.on('this-is-canvas-state',(state)=>{
+        socket.broadcast.emit('canvas-state-from-server',state) 
+      })
+
+
+    console.log('connected')
+    socket.on('draw-line',({prevPoint,currentPoint,color}:Drawline)=>{
+        socket.broadcast.emit('draw-line',{prevPoint,currentPoint,color})
+    })
+
+    socket.on('clear',()=>io.emit('clear'))
+  });
+  
+server.listen(3001,()=>{
+    console.log('listening on 3001')
+})
